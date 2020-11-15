436292beebd1064c34780a7bc77c80c6491c0c5d
Gnico <gnico_10@hotmail.com>
2020-11-15 16:14:28 -0300

    Agregado de algunas interfaces y separación de Widgets.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:health_safe/src/screens/InicioPaciente.dart';
+import 'package:health_safe/src/screens/InicioProfesional.dart';
+import 'package:health_safe/src/screens/ListaProfesionales.dart';
+import 'package:health_safe/src/screens/SacarTurno.dart';
 import 'screens/InicioSesion.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,13 +10,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Health Safe', //título de la aplicación al minimizar.
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         initialRoute: "/",
         routes: {
-          "/": (BuildContext context) => InicioSesion(),
+          "/": (BuildContext context) => InicioPaciente(), //InicioSesion(),
+          "/inicioPaciente": (BuildContext context) => InicioPaciente(),
+          "/inicioProfesional": (BuildContext context) => InicioProfesional(),
+          "/sacarTurno": (BuildContext context) => SacarTurno(),
+          "/listaProfesionales": (BuildContext context) => ListaProfesionales(),
         });
   }
 }
---
+++ lib/src/screens/InicioPaciente.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:health_safe/src/widgets/appBarUsuario.dart';
+import 'package:health_safe/src/widgets/drawerPaciente.dart';
+
+class InicioPaciente extends StatelessWidget {
+  const InicioPaciente({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarUsuario(context),
+      drawer: drawerPaciente(context),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              "¡Bienvenido a Health Safe!",
+              style: TextStyle(fontSize: 25),
+            ),
+            SvgPicture.asset(
+              'assets/svg/HealSafeLogo.svg',
+              height: 220,
+              color: Colors.blue,
+            ),
+            Text(
+              "Estamos aquí para cuidarte.",
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              color: Colors.lightBlue[800],
+              child: Text(
+                "Sacar un turno",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              onPressed: () => {},
+            ),
+            RaisedButton(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+              color: Colors.red[800],
+              child: Text(
+                "Solicitar asistencia inmediata",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
